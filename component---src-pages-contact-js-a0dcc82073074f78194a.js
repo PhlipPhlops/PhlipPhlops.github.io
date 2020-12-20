02c08bd106ff7df1fc1b39239ab1e0af41ec732d
PK Morgan <morgana@localhost.localdomain>
2020-12-20 18:49:56 -0500

    Test gatsby site

---
+++ component---src-pages-contact-js-a0dcc82073074f78194a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"Cuy+":function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return c}));var r=e("q1tI"),u=e.n(r),a=e("LbRr");function c(){return u.a.createElement(a.a,{headerText:"This is the contact page"})}},LbRr:function(n,t,e){"use strict";e.d(t,"a",(function(){return a}));var r=e("q1tI"),u=e.n(r);function a(n){return u.a.createElement("h1",null,n.headerText)}}}]);
+//# sourceMappingURL=component---src-pages-contact-js-a0dcc82073074f78194a.js.map
